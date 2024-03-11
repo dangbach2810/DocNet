9746c2b29ee30178c10e42e5c87c0bdc3eb7872b
Bach Loc Dang <intern.ldbach@tma.com.vn>
2024-03-11 17:54:46 +0700

    git init project real

---
+++ GUI/frmLogin.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BUS;
+namespace GUI
+{
+    public partial class frmLogin : Form
+    {
+        BUS_Employee busEmployee = new BUS_Employee();
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+        
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (txtEmail.Text != "" && txtPassword.Text != "")
+            {
+                if (busEmployee.Login(txtEmail.Text, txtPassword.Text))
+                {
+
+                    MessageBox.Show("Đăng nhập thành công!!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    Properties.Settings.Default.isSave = tglRememberMe.Checked;
+                    if (tglRememberMe.Checked)
+                    {
+                        Properties.Settings.Default.email = txtEmail.Text;
+                        Properties.Settings.Default.password = txtPassword.Text;
+                    }
+                    Properties.Settings.Default.Save();
+                    /*frmMain fMain = new frmMain(txtEmail.Text);
+                    this.Hide();
+                    fMain.ShowDialog();
+                    this.Show();*/
+                }
+                else
+                {
+                    MessageBox.Show("Sai email hoặc mật khẩu!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txtEmail.Text = "";
+                    txtPassword.Text = "";
+                    txtEmail.Focus();
+                }
+            }
+            else if (txtEmail.Text == "" && txtPassword.Text == "")
+            {
+                MessageBox.Show("Không được để trống thông tin!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtEmail.Text = "";
+                txtPassword.Text = "";
+                txtEmail.Focus();
+            }
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+           /* if (Properties.Settings.Default.isSave)
+            {
+                txtEmail.Text = Properties.Settings.Default.email;
+                txtPassword.Text = Properties.Settings.Default.password;
+                tglRememberMe.Checked = true;
+            }*/
+        }
+
+        private void lblForgotPassword_Click(object sender, EventArgs e)
+        {
+            /*if (txtEmail.Text != "")
+            {
+                busEmployee = new BUS_Employee();
+                if (busEmployee.IsExistEmail(txtEmail.Text))
+                {
+                    string password = busEmployee.GetRandomPassword();
+                    if (busEmployee.UpdatePassword(txtEmail.Text, password))
+                    {
+                        SendMail loader = new SendMail(txtEmail.Text, password, true);
+                        loader.ShowDialog();
+                        MessageBox.Show(loader.Result, "Thông báo");
+                    }
+                    else
+                        MessageBox.Show("Không thực hiện được", "Thông báo");
+                }
+            }*/
+        }
+    }
+}
